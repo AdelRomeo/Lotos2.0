717c2e13b60828773568b20323cadc29dc948b17
AdelRomeo <11cage11@mail.ru>
2020-07-16 22:11:37 +0700

    логика переключения страниц

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,28 @@
 let container = document.querySelector('.main-container');
+let nextPage = document.querySelector('.next-page');
+
+let pageNumb = 2; //счетчик для изменения страницы
+
+let page = 'section' + pageNumb + '.html'; //страница которая подставляется в fetch
+
+nextPage.addEventListener('click', ()=>{
+    fetch(page)
+        .then((response)=>{
+            if (response.status === 404){
+                return Promise.reject();
+            }
+            return response.text()
+        })
+        .then((text)=>{
+            container.innerHTML = text;
+            pageNumb = pageNumb + 1; // увелечение счетчика для страницы
+            page = 'section' + pageNumb + '.html'; // изменение страницы
+            console.log(pageNumb);
+            console.log(page);
+        });
+});
+
+
 
 fetch('section1.html')
     .then((response)=>{
