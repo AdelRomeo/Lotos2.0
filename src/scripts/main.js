669b045b21c7e58bfbe576c13c14f10d1b00ee8b
AdelRomeo <11cage11@mail.ru>
2020-07-25 20:27:41 +0700

    full page section2 + dymic script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,25 +17,15 @@ nextPage.addEventListener('click', ()=>{
             container.innerHTML = text;
             pageNumb = pageNumb + 1; // увелечение счетчика для страницы
             page = 'section' + pageNumb + '.html'; // изменение страницы
+
+            let section = document.querySelector('.section'); //секция с подгружаемым контентом
+            section.style.maxHeight = maxHeightSection(); //начальный размер страницы
+
+            let script = document.createElement('script'); //создаем элемент скрипт
+            script.src = "src/scripts/section2.js"; // путь до нужного скрипта
+            document.body.append(script); // добавляем скрипт в боди
         });
 });
-
-
-let obj = {
-    fn1 : function() {
-        let mainHeader = document.querySelector('.main-header'); //header
-        let mainHeaderHeight = mainHeader.clientHeight; //высота header
-
-        let mainFooter = document.querySelector('.main-footer'); //footer
-        let mainFooterHeight = mainFooter.clientHeight; //высота footer
-
-        let section = document.querySelector('.section'); //секция с подгружаемым контентом
-        let maxHeeight = document.documentElement.clientHeight; // высота всей страницы (body)
-
-        return section.style.maxHeight = maxHeeight - mainHeaderHeight - mainFooterHeight + 'px'; //задаем высоты секции (высота страницы
-        // -header и footer)
-    }
-};
 
 function maxHeightSection(){ // универсальная функция которая возвращает высоты для section
     let mainHeader = document.querySelector('.main-header'); //header
@@ -62,7 +52,7 @@ fetch('section1.html')
         container.innerHTML = text; // закидываем на страницу подгруженный контент
 
         let section = document.querySelector('.section'); //секция с подгружаемым контентом
-        section.style.maxHeight = maxHeightSection();
+        section.style.maxHeight = maxHeightSection(); //начальный размер страницы
 
         window.addEventListener('resize', ()=>{ // resize - изменение размера окна
             section.style.maxHeight = maxHeightSection();
