02cf86042413d28faf7254fd0c7973f0d8c6f625
AdelRomeo <11cage11@mail.ru>
2020-07-30 19:04:02 +0700

    full nav html+scss+js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,9 +3,9 @@ let nextPage = document.querySelector('.next-page_btn');
 
 let pageNumb = 2; //счетчик для изменения страницы
 
-let page = 'section' + pageNumb + '.html'; //страница которая подставляется в fetch
-
 nextPage.addEventListener('click', ()=>{
+    let page = 'section' + pageNumb + '.html'; //страница которая подставляется в fetch
+
     fetch(page)
         .then((response)=>{
             if (response.status === 404){
@@ -35,6 +35,7 @@ nextPage.addEventListener('click', ()=>{
             page = 'section' + pageNumb + '.html'; // изменение страницы
         });
 });
+
 
 function maxHeightSection(){ // универсальная функция которая возвращает высоты для section
     let mainHeader = document.querySelector('.main-header'); //header
@@ -68,3 +69,52 @@ fetch('section1.html')
         });
     });
 //----------------------------------------------
+
+//-- открытие и подгрузка контента по кликами в верхнем меню
+let menu = document.querySelector('.header_menu'); //кнопка по которой кликают
+
+menu.addEventListener('click', ()=>{ //клик пок нопке
+   let menuList = document.querySelector('.main-nav_list'); //список меню
+
+   menuList.classList.toggle('vision-menu'); //переключение списка видимости меню
+   menu.classList.toggle('header_menu-affect'); //анимацию бурнера меню
+});
+
+let menuListItem = document.querySelectorAll('.main-nav_item'); //элементы списка меню (li`шки)
+
+for (let i=0; i<menuListItem.length; i++){ //перебираем все лишки
+    menuListItem[i].addEventListener('click', ()=>{ //кликаем по лишке
+
+        let counter = i + 1; //увеличиваем i чтобы не было нуля
+        let page = 'section' + counter +'.html'; //составляем адрес страницы для подрузки
+
+        fetch(page)
+            .then((response)=>{
+                if (response.status === 404){
+                    return Promise.reject();
+                }
+                return response.text()
+            })
+            .then((text)=>{
+                container.innerHTML = text;
+
+                let section = document.querySelector('.section'); //секция с подгружаемым контентом
+                section.style.maxHeight = maxHeightSection(); //начальный размер страницы
+
+                if (page === 'section2.html'){
+                    let script = document.createElement('script'); //создаем элемент скрипт
+                    script.src = "src/scripts/section2.js"; // путь до нужного скрипта
+                    section.appendChild(script); // добавляем скрипт в секцию
+                }
+
+                if (page === 'section4.html'){
+                    let script = document.createElement('script'); //создаем элемент скрипт
+                    script.src = "src/scripts/section4.js"; // путь до нужного скрипта
+                    section.appendChild(script); // добавляем скрипт в секцию
+                }
+
+                pageNumb = counter+1; // изменение счетчика для страницы
+                page = 'section' + pageNumb + '.html'; // изменение страницы
+            });
+    })
+}
