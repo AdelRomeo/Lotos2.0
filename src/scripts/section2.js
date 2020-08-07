ce6b56a1f15144bf2f9e512f19a099ccbd80d3ad
AdelRomeo <11cage11@mail.ru>
2020-08-07 19:30:41 +0700

    gull page section5 html + scss and antiDoubleVariables function

--- src/scripts/section2.js
+++ src/scripts/section2.js
@@ -1,13 +1,14 @@
-let list = document.querySelector('.section2_list-variety'); //список
-let listLi = document.querySelectorAll('.section2_list-variety>li'); //li`шки
-let listMarker = document.querySelector('.section2_list-marker'); //'Yoga'
+(function antiDoubleVariables() {
+    let list = document.querySelector('.section2_list-variety'); //список
+    let listLi = document.querySelectorAll('.section2_list-variety>li'); //li`шки
+    let listMarker = document.querySelector('.section2_list-marker'); //'Yoga'
 
-list.addEventListener('mouseover', (e)=>{ //вешаем обработчик на список на наведение мыши
-    for (let i=0; i<listLi.length; i++){ //перебираем все li`шки
-        if (e.target === listLi[i]){ //если элемент на который навели мышь это li`шка
-            listMarker.style.top = e.target.offsetTop + 'px'; //присваиваем маркеру значение по вертикали равному li`шке на которую
-            // навели мышь
+    list.addEventListener('mouseover', (e)=>{ //вешаем обработчик на список на наведение мыши
+        for (let i=0; i<listLi.length; i++){ //перебираем все li`шки
+            if (e.target === listLi[i]){ //если элемент на который навели мышь это li`шка
+                listMarker.style.top = e.target.offsetTop + 'px'; //присваиваем маркеру значение по вертикали равному li`шке на которую
+                // навели мышь
+            }
         }
-    }
-});
-
+    });
+})();
