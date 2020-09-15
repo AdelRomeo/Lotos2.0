e198197ac23944109132c2c4a2c08333d415b330
AdelRomeo <11cage11@mail.ru>
2020-09-15 22:01:18 +0700

    full adaptive page section7

--- src/scripts/section7.js
+++ src/scripts/section7.js
@@ -11,4 +11,36 @@
             }
         }
     });
+
+    //подгрузка выбранного элемента в мобильной версии
+    let leftBtn = document.querySelector('.section2_move-left'); //кнопка влево
+    let rightBtn = document.querySelector('.section2_move-right'); //кнопка вправо
+    let btnCont = document.querySelector('.section2_move-cont'); //контейнер в котором находятся кнопки
+    let section2Container = document.querySelectorAll('.section7_client-info');
+    let counter = 0; //начальное состояния счетчика для смены элементов
+    leftBtn.setAttribute("disabled", "disabled");//состояние левой кнопки по умолчанию
+
+    btnCont.addEventListener('click', (e)=>{ //вешаем обработчик на контейнер
+        console.log(1);
+        if (e.target === leftBtn){ //если нажали на левую кнопку
+            counter = counter -1; //изменение счетчика
+        }
+        if (e.target === rightBtn){ //если нажали на правую кнопку
+            counter = counter +1; //изменение счетчика
+        }
+        for (let i = 0; i<section2Container.length; i++){ //перебираем все элементы для подгрузки
+            section2Container[i].style.display = 'none'; //скрываем все элементы
+            section2Container[counter].style.display = 'block'; //показываем элемент номер которого равен counter
+        }
+        if (counter >= section2Container.length -1){ //ограничение counter`a
+            rightBtn.setAttribute("disabled", "disabled"); //отключение кнопки если значения counter`a НЕкорректно
+        } else {
+            rightBtn.removeAttribute("disabled"); //включение кнопки если значения counter`a корректно
+        }
+        if (counter <= 0){ //ограничение counter`a
+            leftBtn.setAttribute("disabled", "disabled"); //отключение кнопки если значения counter`a НЕкорректно
+        } else {
+            leftBtn.removeAttribute("disabled"); //включение кнопки если значения counter`a корректно
+        }
+    })
 })();
